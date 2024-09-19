54c01549e1a4f7bb83734cbe98a6edf3ee55ea85
MarcoManjarrez <142528062+MarcoManjarrez@users.noreply.github.com>
2024-09-19 10:25:41 -0600

    Update file6.cpp

--- Project1/Project1/file6.cpp
+++ Project1/Project1/file6.cpp
@@ -10,13 +10,17 @@ using namespace std;
 
 void showImage(Mat image);
 
-void histogram(int cols, int rows, Mat image);
+void histogram(int cols, int rows, Mat image, int counter[]);
+void equalization(int pixelValues[], int cols, int rows, Mat image, Mat equalizedImage);
 
 int main() {
+	int counter[256] = { 0 };
 	int columns, rows;
 	Mat image, image2, image3;
-	image = imread("therealoppenheimer.jpg", IMREAD_GRAYSCALE);
+	//image = imread("therealoppenheimer.jpg", IMREAD_GRAYSCALE);
 	image2 = imread("flower.jpg", IMREAD_GRAYSCALE);
+	
+	image = imread("darkImage1.jpg", IMREAD_GRAYSCALE);
 
 	if (image.cols < image2.cols) {
 		columns = image.cols;
@@ -31,18 +35,13 @@ int main() {
 	else {
 		rows = image2.rows;
 	}
+
+	Mat equalizedImage(rows, columns, CV_8U);
+
 	
-	
-	/*
-	createMask(columns, rows, image2, image);
-	addImage(columns, rows, image, image2);
-	constantImage(image);
-	functionImage(image);
-	substractImage(columns, rows, image, image2 );
-	addImagePercentage(columns, rows, 60, 40, image, image2);
-	imageNegative(columns, rows, image);
-	*/
-	histogram(columns, rows, image);
+	histogram(columns, rows, image, counter);
+	equalization(counter, columns, rows, image, image2);
+	histogram(columns, rows, equalizedImage, counter);
 }
 
 void showImage(Mat image) {
@@ -51,8 +50,8 @@ void showImage(Mat image) {
 	waitKey(0);
 }
 
-void histogram(int cols, int rows, Mat image) {
-	int counter[256] = { 0 };
+void histogram(int cols, int rows, Mat image, int counter[]) {
+	//int counter[256] = { 0 };
 	int aux = 0;
 	Mat addedImage(512, 512, CV_8U);
 
@@ -87,4 +86,47 @@ void histogram(int cols, int rows, Mat image) {
 	showImage(addedImage);
 
 	waitKey(0);
+}
+
+void equalization(int pixelValues[], int cols, int rows, Mat image, Mat equalizedImage) {
+	
+	float pmf[256] = { 0 };
+	float cdf[256] = { 0 };
+	for (int i = 0; i < 256; i++) {
+		pmf[i] = (float) pixelValues[i] / (cols * rows);
+	}
+
+	for (int i = 0; i < 256; i++) {
+		cout << "PMF " << pmf[i] << endl;
+	}
+
+	for (int i = 0; i < 256; i++) {
+		cdf[i] = pmf[i];
+	}
+
+	for (int i = 0; i < 256; i++) {
+		if (i > 0) {
+			cdf[i] = cdf[i] + cdf[i - 1];
+		}
+		else {
+			cdf[i] = pmf[i];
+		}
+	}
+
+	for (int i = 0; i < 256; i++) {
+		cout << "CDF " << cdf[i] << endl;
+	}
+
+	for (int i = 0; i < cols; i++) {
+		for (int j = 0; j < rows; j++) {
+			uchar val = image.at<uchar>(j, i);
+			uchar valCdf = (uchar)(255 * cdf[val]);
+			equalizedImage.at<uchar>(j, i) = valCdf;
+		}
+	}
+
+	//histogram(cols, rows, equalizedImage, counter);
+	
+	showImage(equalizedImage);
+	showImage(image);
 }
