4923a9922c6520af2921676c8f6db0b8217b2f42
MarcoManjarrez <142528062+MarcoManjarrez@users.noreply.github.com>
2024-10-10 10:41:12 -0600

    Update file10.cpp

--- Project1/Project1/file10.cpp
+++ Project1/Project1/file10.cpp
@@ -2,21 +2,23 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
-#include <opencv2/videoio.hpp>
 #include <algorithm>
 #include <cmath>
+#include <queue>
 
 using namespace cv;
 using namespace std;
 
 void erosion(Mat image);
 void dilation(Mat image);
+void growing(Mat image);
 
 int main() {
 	Mat image;
 	image = imread("pigs.jpg", IMREAD_GRAYSCALE);
-	erosion(image);
-	dilation(image);
+	//erosion(image);
+	//dilation(image);
+	growing(image);
 }
 
 
@@ -96,4 +98,62 @@ void dilation(Mat image) {
 	imshow("Dilated image", dilatedImage);
 
 	waitKey(0);
+}
+
+void growing(Mat image) {
+	queue<Point> pixels;
+	Mat newImage(image.rows, image.cols, CV_8U);
+	Mat checkMatrix(image.rows, image.cols, CV_8U, Scalar(0));
+	int cont = 1;
+	
+
+
+	for (int i = 0; i < image.cols; i++) {
+		for (int j = 0; j < image.rows; j++) {
+			if (image.at<uchar>(j, i) < 128) {
+				newImage.at<uchar>(j, i) = 0;
+			}
+			else {
+				newImage.at<uchar>(j, i) = 255;
+			}
+		}
+	}
+
+	for (int i = 0; i < image.cols; i++) {
+		for (int j = 0; j < image.rows; j++) {
+			if (newImage.at<uchar>(j, i) == 255) 
+			{
+			  if(checkMatrix.at<uchar>(j, i) == 0){
+				checkMatrix.at<uchar>(j,i) = cont;
+				for (int k = -1; k <= 1; k++) {
+					for (int l = -1; l <= 1; l++) {
+						if((i+l) > 0 && (i + l) < image.cols && (j+k) > 0 && (j + k) < image.rows){
+						if (newImage.at<uchar>(i + l, j + k) == 255) pixels.push(Point(i+l, j+k));
+						}
+					}
+				}
+				while (!pixels.empty()) {
+					if (newImage.at<uchar>(pixels.front().x, pixels.front().y) == 255 && checkMatrix.at<uchar>(pixels.front().x, pixels.front().y) == 0) {
+						checkMatrix.at<uchar>(pixels.front().x, pixels.front().y) = cont;
+						pixels.pop();
+						for (int k = -1; k <= 1; k++) {
+							for (int l = -1; l <= 1; l++) {
+								if ((i + l) > 0 && (i + l) < image.cols && (j + k) > 0 && (j + k) < image.rows) {
+									if (newImage.at<uchar>(i + l, j + k) == 255) pixels.push(Point(i + l, j + k));
+								}
+							}
+						}
+					}
+				}
+			  }
+			}
+			cont++;
+		}
+	}
+
+	namedWindow("Binary", WINDOW_NORMAL);
+	imshow("Binary", newImage);
+	namedWindow("Regioned image", WINDOW_AUTOSIZE);
+	imshow("Regioned image", checkMatrix);
+
 }
