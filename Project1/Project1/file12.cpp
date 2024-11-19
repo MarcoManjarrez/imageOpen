8bf829a1fda067f0e4851ce151442fa60fd98b50
MarcoManjarrez <142528062+MarcoManjarrez@users.noreply.github.com>
2024-11-19 10:35:12 -0600

    Update file12.cpp

--- Project1/Project1/file12.cpp
+++ Project1/Project1/file12.cpp
@@ -12,6 +12,8 @@ void scaling(int cols, int rows, int scalingFactorX, int scalingFactorY, Mat ima
 void smoothScaling(int cols, int rows, float scalingFactorX, float scalingFactorY, Mat image);
 void translation(int cols, int rows, int translationX, int translationY, Mat image);
 void rotation(int cols, int rows, float angle, Mat image);
+void matrixTranslation(int cols, int rows, int translationX, int translationY, Mat image);
+void matrixRotation(int cols, int rows, int angle, Mat image);
 
 int main() {
     Mat image;
@@ -23,8 +25,11 @@ int main() {
     waitKey(10);
 
     //scaling(image.cols, image.rows, 2, 2, image);
-    smoothScaling(image.cols, image.rows, 5, 8, image);
+    //smoothScaling(image.cols, image.rows, 2, 3, image);
     //translation(image.cols, image.rows, -50, 50, image);
+    //rotation(image.cols, image.rows, 30, image);
+    //matrixTranslation(image.cols, image.rows, -50, 50, image);
+    matrixRotation(image.cols, image.rows, CV_PI/2, image);
 }
 
 void scaling(int cols, int rows, int scalingFactorX, int scalingFactorY, Mat image) {
@@ -124,7 +129,8 @@ void translation(int cols, int rows, int translationX, int translationY, Mat ima
 
 void rotation(int cols, int rows, float angle, Mat image) {
     Mat rotatedImage(rows, cols, CV_8U);
-    angle = (angle * 180) / (3.14);
+    angle = (angle * 180) / CV_PI;
+    int Q11 = 0, Q21 = 0, Q12 = 0, Q22 = 0, R1 = 0, R2 = 0, P = 0;
     for (int i = 0; i < cols; i++) {
         for (int j = 0; j < rows; j++) {
             float x0 = i - (cols / 2);
@@ -133,9 +139,99 @@ void rotation(int cols, int rows, float angle, Mat image) {
             float v0 = -x0 * sin(angle) + y0 * cos(angle);
             float u = u0 + cols / 2;
             float v = (rows / 2) - v0;
+            if (ceil(u) >= 0 && ceil(u) < rows && floor(u) >= 0 && floor(u) < rows && ceil(v) >= 0 && ceil(v) < cols && floor(v) >= 0 && floor(v) < cols) {
+
+                Q11 = image.at<uchar>(floor(u), floor(v));
+
+                Q21 = image.at<uchar>(ceil(u), floor(v));
+                Q12 = image.at<uchar>(floor(u), ceil(v));
+                Q22 = image.at<uchar>(ceil(u), ceil(v));
+
+
+                if (floor(u) == u && floor(v) == v) {
+                    rotatedImage.at<uchar>(i, j) = image.at<uchar>(u, v);
+                }
+                else if (floor(u) == u && floor(v) != v) {
+                    R1 = Q11;
+                    R2 = Q12;
+                    P = ((float)(R2 - R1) / ((i + 1) - (i))) * (v - floor(v)) + R1;
+                    rotatedImage.at<uchar>(i, j) = (uchar)P;
+                }
+                else if (floor(u) != u && floor(v) == v) {
+                    R1 = Q11;
+                    R2 = Q21;
+                    P = ((float)(R2 - R1) / ((i + 1) - (i))) * (v - floor(v)) + R1;
+                    rotatedImage.at<uchar>(i, j) = (uchar)P;
+                }
+                else {
+                    R1 = ((float)(Q21 - Q11) / ((j + 1) - (j))) * (u - floor(u)) + Q11;
+                    R2 = ((float)(Q22 - Q12) / ((j + 1) - (j))) * (u - floor(u)) + Q12;
+                    P = ((float)(R2 - R1) / ((i + 1) - (i))) * (v - floor(v)) + R1;
+                    rotatedImage.at<uchar>(i, j) = (uchar)P;
+                }
+            }
         }
     }
 
+    namedWindow("Rotated image", WINDOW_AUTOSIZE);
+    imshow("Rotated image", rotatedImage);
+
+    waitKey(0);
+}
+
+
+void matrixTranslation(int cols, int rows, int translationX, int translationY, Mat image) {
+    Mat T = (Mat_<float>(3, 3) << 1, 0, translationX, 0, 1, translationY, 0, 0, 1);
+    Mat v(3,1, CV_32F);
+    Mat translatedImage(rows, cols, CV_8U);
+
+    for (int i = 0; i < cols; i++) {
+        for (int j = 0; j < rows; j++) {
+            v.at<float>(0, 0) = i;
+            v.at<float>(1, 0) = j;
+            v.at<float>(2, 0) = 1;
+            Mat res = T * v;
+            float u = res.at<float>(0, 0);
+            float v = res.at<float>(1, 0);
+            if (u > 0 && u < cols && v > 0 && v < rows) {
+                translatedImage.at<uchar>(j, i) = image.at<uchar>(v, u);
+            }
+            else {
+                translatedImage.at<uchar>(j, i) = (uchar)0;
+            }
+        }
+    }
+    namedWindow("Translated image", WINDOW_AUTOSIZE);
+    imshow("Translated image", translatedImage);
+
+    waitKey(0);
+}
+
+void matrixRotation(int cols, int rows, int angle, Mat image) {
+    Mat T1 = (Mat_<float>(3, 3) << cos(angle), sin(angle), 0, -sin(angle), cos(angle), 0, 0, 0, 1);
+    Mat T2 = (Mat_<float>(3, 3) << 1, 0, (cols/2), 0, 1, (rows/2), 0, 0, 1);
+    Mat T3 = (Mat_<float>(3, 3) << 1, 0, -(cols/2), 0, 1, -(rows/2), 0, 0, 1);
+    Mat T;
+    Mat v(3, 1, CV_32F);
+    Mat rotatedImage(rows, cols, CV_8U);
+
+    for (int i = 0; i < cols; i++) {
+        for (int j = 0; j < rows; j++) {
+            v.at<float>(0, 0) = i;
+            v.at<float>(1, 0) = j;
+            v.at<float>(2, 0) = 1;
+            T = T2 * T1 * T3;
+            Mat res = T * v;
+            float u = res.at<float>(0, 0);
+            float v = res.at<float>(1, 0);
+            if (u > 0 && u < cols && v > 0 && v < rows) {
+                rotatedImage.at<uchar>(j, i) = image.at<uchar>(v, u);
+            }
+            else {
+                rotatedImage.at<uchar>(j, i) = (uchar)0;
+            }
+        }
+    }
     namedWindow("Rotated image", WINDOW_AUTOSIZE);
     imshow("Rotated image", rotatedImage);
 
